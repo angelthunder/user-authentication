d5f26e4f9d3a57ce8cf321a0fa5451b0972e0eda
Angel Szymczak <angel.szymczak@Marte.local>
2023-10-31 11:44:21 -0300

    feat: Add Login User access

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,22 @@
+class LoginsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(login_params)
+
+    if @user.present?
+      redirect_to dashboards_path
+    else
+      flash[:alert] = "email or password is wrong!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def login_params
+    params.require(:user).permit(:email, :password)
+  end
+end
