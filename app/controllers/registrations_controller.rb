cbcb7f1577ae0a9ec5c37f1356ee8b49a9f39733
Angel Szymczak <angel.szymczak@hotmail.com>
2023-11-08 12:39:59 -0300

    feat: Add endpoint for create and store a new user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,22 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      flash[:notice] = "successful registration"
+      redirect_to(new_login_path)
+    else
+      flash[:alert] = "couldn't save registration"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
