5a7784f633c608b93e14dcfe033fa992d0d15a70
Angel Szymczak <angel.szymczak@hotmail.com>
2023-11-11 10:49:34 -0300

    feat: Triggering Turbo Streams from Controller Broadcast

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -15,6 +15,23 @@ class SiteController < ApplicationController
 
   def six
     @name, @email, @age = params[:person].values_at(:name, :email, :age)
+    author = User.create_with(password: "123").find_or_create_by(email: @email)
+
     @count = params[:count].to_i + 1
+    @post = author.posts.create(
+      title: "Automated Test Post from #{@name}:#{@age}",
+      body: "This is #{Time.now}",
+    )
+
+    # @post.broadcast_prepend_to(
+    #   "teststr",
+    #   target: "broadcasts",
+    #   html: "<div>NEW POST: #{@post.title}</div>"
+    # )
+    # if you don't send the :html content,
+    # it will look for a partial with the same instance @post.
+    Rails.logger.info("BEFORE CONTROLLER - Broadcast Prepend to")
+    @post.broadcast_prepend_to("teststr", target: "broadcasts")
+    Rails.logger.info("AFTER CONTROLLER - Broadcast Prepend to")
   end
 end
