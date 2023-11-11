a83a25c5e7819cbb4802625d8ba1f3dd8caa4d1d
Angel Szymczak <angel.szymczak@hotmail.com>
2023-11-11 12:01:17 -0300

    feat: Read input value to `text` property from Test controller

--- app/javascript/controllers/test_controller.js
+++ app/javascript/controllers/test_controller.js
@@ -2,11 +2,14 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="test"
 export default class extends Controller {
+  static targets = ["text"];
+
   connect() {
     console.log("TestController: Connected!");
   }
 
   hello() {
     console.log("TestController: Button Clicked, Hello!");
+    console.log("TestController: You typed: " + this.textTarget.value);
   }
 }
