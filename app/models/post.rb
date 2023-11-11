44030387f9b1895ed11f9f39a2ac654da1d0748c
Angel Szymczak <angel.szymczak@hotmail.com>
2023-11-11 10:49:21 -0300

    feat: Triggering Turbo Streams from Model Broadcast

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,34 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
+  after_create_commit do
+    Rails.logger.info("AFTER CREATE COMMIT - Broadcast Update to")
+    broadcast_update_to(
+      "teststr",
+      target: "broadcasts",
+      html: <<-HTML
+        <div style="background-color: green">
+          <h5>AFTER CREATE COMMIT Broadcast</h5>
+          <p>Post #{title} was created</p>
+        </div>
+      HTML
+    )
+  end
+
+  after_update_commit do
+    Rails.logger.info("AFTER UPDATE COMMIT - Broadcast Prepend to")
+    broadcast_prepend_to(
+      "teststr",
+      target: "broadcasts",
+      html: <<-HTML
+        <div style="background-color: blue">
+          <h5>AFTER UPDATE COMMIT Broadcast</h5>
+          <p>Changed title to #{title}</p>
+        </div>
+      HTML
+    )
+  end
+
   private
 
   def eql?(other)
